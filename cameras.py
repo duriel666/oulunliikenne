61fbf3b939d8e63d46d8de67b5e413adb5dcfef8
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-12-16 19:41:31 +0200

    edit

--- cameras.py
+++ cameras.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import os
+import time
 
 q_address = 'https://api.oulunliikenne.fi/proxy/graphql'
 query = 'query {cameras {cameraId,name,lat,lon,presets {presetId,presentationName,imageUrl,measuredTime}}}'
@@ -32,9 +33,14 @@ with open('cameras.json') as f:
 
 for url in url_list:
     _, file_path = os.path.split(url)
-    if not os.path.exists(file_path):
+    response = requests.get(url)
+    current_time = time.strftime('%Y%m%d%H%M%S', time.localtime())
+    name = file_path.split('.')[0]+'_'+current_time+'.'+file_path.split('.')[1]
+    with open(name, 'wb') as f:
+        f.write(response.content)
+        print(f'Saved {name}')
+    '''if not os.path.exists(file_path) or os.path.getsize(file_path) == int(url.headers['Content-Length']):
         response = requests.get(url)
-
         with open(file_path, 'wb') as f:
             f.write(response.content)
-            print(f'Saved {file_path}')
+            print(f'Saved {file_path}')'''
