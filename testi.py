616f93c51c3719e29666150449c73117f6735f4f
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-12-28 08:54:44 +0200

    edit

--- testi.py
+++ testi.py
@@ -2,11 +2,15 @@ import time
 
 run = True
 i = 1
+total = 0
+total_time = time.time()
 while run:
     start_time = time.time()
     for j in range(2, i):
         if i % j == 0:
             break
     else:
-        print(f'{i} - {time.time() - start_time:.4f} s since last prime')
+        total += 1
+        print(
+            f'{i} - {time.time() - start_time:.4f} s since last prime - {total} primes found - {time.time() - total_time:.2f} s total time')
     i += 1
