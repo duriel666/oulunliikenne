bec00d712510675542a187d3fff0d194eb7cfc25
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-12-28 08:50:20 +0200

    edit

--- testi.py
+++ testi.py
@@ -1,12 +1,12 @@
 import time
 
 run = True
-i = 1000000
+i = 1
 while run:
     start_time = time.time()
     for j in range(2, i):
         if i % j == 0:
             break
     else:
-        print(i, time.time() - start_time)
+        print(f'{i} - {time.time() - start_time:.4f} s since last prime')
     i += 1
