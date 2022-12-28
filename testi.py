5fc2f4046d059aee89899b3e14da12cf935e6b48
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-12-28 08:32:24 +0200

    edit

--- testi.py
+++ testi.py
@@ -1,3 +1,9 @@
-for i in range(1, 101):
-    if i % 2 != 0:
+run = True
+i = 2
+while run:
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
         print(i)
+    i += 1
