06842d25d96abe542d6a4afc310c1870445b15f4
duriel666 <59049578+duriel666@users.noreply.github.com>
2023-01-04 02:50:15 +0200

    edit

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from weather import *
 import time
 
 
+class Main:
+    def __init__(self, started):
+        self.start_list = started
+
+    def start(self):
+            self.start()
+            time.sleep(1)
+
+
 def main():
     threads = [cameras_start, lam_start, parking_start, weather_start]
     for thread in threads:
-        thread = Thread(target=thread)
-        thread.start()
-        time.sleep(5)
+        t = Main(thread)
+        t.start()
 
 
 if __name__ == '__main__':
