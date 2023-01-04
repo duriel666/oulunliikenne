0ee7cf5ea32fc3b3efad77312c1d081cf3835b59
duriel666 <59049578+duriel666@users.noreply.github.com>
2023-01-04 02:42:50 +0200

    edit

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ from parking import *
 from weather import *
 import time
 
+
+def main():
+    threads = [cameras_start, lam_start, parking_start, weather_start]
+    for thread in threads:
+        thread = Thread(target=thread)
+        thread.start()
+        time.sleep(5)
+
+
 if __name__ == '__main__':
-    t1 = Thread(target=cameras_start)
-    t2 = Thread(target=lam_start)
-    t3 = Thread(target=parking_start)
-    t4 = Thread(target=weather_start)
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    print('All threads finished')
-    time.sleep(1)
+    main()
