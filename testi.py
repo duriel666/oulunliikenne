a075ce3d75d4f668a275dae7a3a4604318b17291
duriel666 <59049578+duriel666@users.noreply.github.com>
2023-01-06 19:58:46 +0200

    edit

--- testi.py
+++ testi.py
@@ -14,13 +14,3 @@ while run:
         print(
             f'{i} - {time.time() - start_time:.4f} s since last prime - {total} primes found - {time.time() - total_time:.2f} s total time')
     i += 1
-
-#q: capital of finland?
-#a: helsinki
-
-#q: python code to calculate sum of nu,bers up to x?
-#a: sum(range(x+1))
-
-#q: python code to calculate square root of x?
-#a: x**0.5
-
